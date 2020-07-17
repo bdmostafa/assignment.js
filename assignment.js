f36df0ba8e02e36d17d0416b23a93e06e5de9377
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-17 08:05:41 +0600

    assignment - requirements/tasks listed

---
+++ assignment.js
@@ -0,0 +1,47 @@
+/* 1. Feet-To-Mile converter ==========================
+Requirements & tasks to be done:
+    input - feet,
+    negative feet not allowed,
+    convert feet to mile,
+    if float, 2 digit after decimal (35.54)
+    output - mile
+ */
+function feetToMile() {
+
+}
+
+/* 2. Wood Calculator in chair, table and bed ==================================
+Requirements & tasks to be done:
+    input - chair, table and bed in chronological order,
+    1 chair = 1 cubic feet, 1 table = 3 cubic feet, 1 bed = 5 cubic feet,
+    negative value not allowed,
+    output - cubic feet
+*/
+function woodCalculator() {
+
+}
+
+/* 3. Brick Calculator in building ==============================
+Requirements & tasks to be done:
+    input - building floors,
+    negative floor not allowed,
+    count floors & compare,
+    (1 - 10) floors height - 15 feet, (11 - 20) floors height - 12 feet, (21 - ~) floors height - 10 feet,
+    every feet need 1000 bricks constantly,
+    output - bricks number
+*/
+function brickCalculator() {
+
+}
+
+/* 4. Tiny Friend - the lowest friend from an array ================================
+Requirements & tasks to be done:
+    input - an array of friends' list,
+    empty array not allowed,
+    compare the length of every element/string in the array
+    find out the lowest string from them
+    output - the lowest friend
+*/
+function tinyFriend() {
+
+}
